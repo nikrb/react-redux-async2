ae7f99c9fc9a2f0f4d6075f0815260a307e9b636
nikrb <nik_rb@yahoo.com>
2017-12-27 21:00:06 +0000

    centre the list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import ItemList from './components/ItemList';
 import './App.css';
 
 class App extends Component {
   render() {
+    const style = {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center"
+    };
     return (
-      <div className="App">
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={style}>
+        <ItemList />
       </div>
     );
   }
