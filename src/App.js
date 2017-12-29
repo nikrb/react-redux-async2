a0c6dffae43ca9235c28c9ff15e4ea937d2e2b36
nikrb <nik_rb@yahoo.com>
2017-12-29 22:03:30 +0000

    add redux provider to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import {Provider} from 'react-redux';
+import configureStore from './store/configureStore';
 import ItemList from './components/ItemList';
 import './App.css';
+
+const store = configureStore();
 
 class App extends Component {
   render() {
@@ -10,9 +14,11 @@ class App extends Component {
       justifyContent: "center"
     };
     return (
-      <div style={style}>
-        <ItemList />
-      </div>
+      <Provider store={store} >
+        <div style={style}>
+          <ItemList />
+        </div>
+      </Provider>
     );
   }
 }
